112f950a989b4ba01e427d40b9f5df6f0434ad5a
chenxiyong <3287486878@qq.com>
2024-09-23 15:52:41 +0800

    first commit

---
+++ build/CMakeFiles/test_rknn_lib.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tta/pysot2rknn/src/main.cc" "/home/tta/pysot2rknn/build/CMakeFiles/test_rknn_lib.dir/src/main.cc.o"
+  "/home/tta/pysot2rknn/src/postprocess.cc" "/home/tta/pysot2rknn/build/CMakeFiles/test_rknn_lib.dir/src/postprocess.cc.o"
+  "/home/tta/pysot2rknn/src/preprocess.cc" "/home/tta/pysot2rknn/build/CMakeFiles/test_rknn_lib.dir/src/preprocess.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BUILD_VIDEO_RTSP"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../lib/rknn/include/rknn"
+  "../third"
+  "../third/rga/include"
+  "../third/mpp/include"
+  "../third/zlmediakit/include"
+  "../include"
+  "../third/opencv/opencv-linux-aarch64/include"
+  "../third/opencv/opencv-linux-aarch64/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/test_rknn_lib.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_rknn_lib.dir/src/main.cc.o"
+  "CMakeFiles/test_rknn_lib.dir/src/postprocess.cc.o"
+  "CMakeFiles/test_rknn_lib.dir/src/preprocess.cc.o"
+  "test_rknn_lib"
+  "test_rknn_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_rknn_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
